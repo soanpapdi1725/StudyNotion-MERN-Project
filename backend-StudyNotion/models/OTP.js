3b870655644935a10319443d1f9e86ed359f218c
Sonu Yadav <sy239370@gmail.com>
2025-07-22 12:26:41 +0530

    22-07-2025

--- backend-StudyNotion/models/OTP.js
+++ backend-StudyNotion/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const otpSchema = mongoose.Schema({
   forEmail: {
@@ -10,6 +11,24 @@ const otpSchema = mongoose.Schema({
     required: true,
   },
   createdAt: { type: Date, default: Date.now(), expires: 5 * 60 },
+});
+
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verifcation Email From Study Notion",
+      otp
+    );
+    console.log("Email Send Successfully", mailResponse);
+  } catch (error) {
+    console.log("Error while sending verification mail", error);
+  }
+}
+
+otpSchema.pre("save", async function (next) {
+  await sendVerificationEmail(this.email, this.otp);
+  next();
 });
 
 module.exports = mongoose.model("OTP", otpSchema);
