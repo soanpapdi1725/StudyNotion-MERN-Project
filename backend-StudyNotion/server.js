39ea4edf549a2ac450d3578d95f85d2baa7cea9d
Sonu Yadav <sy239370@gmail.com>
2025-08-02 19:44:33 +0530

    02/08/2025 - 3

--- backend-StudyNotion/server.js
+++ backend-StudyNotion/server.js
@@ -5,3 +5,7 @@ const courseRouter = require("./routes/courseRouter");
 const authAndResetRouter = require("./routes/AuthAndResetRouter");
 const paymentRouter = require("./routes/paymentRouter");
 const profileRouter = require("./routes/paymentRouter");
+
+const database = require("./config/database");
+const cloudinary = require("./config/cloudinary");
+const razorpay = require("./config/razorpay");
