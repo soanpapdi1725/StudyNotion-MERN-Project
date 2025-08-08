4df897f8f513137469ec80ad0baeba8b250818de
Sonu Yadav <sy239370@gmail.com>
2025-08-08 23:00:46 +0530

    08/08/2025 -2

---
+++ Frontend/src/components/core/homepage/CodeBlock.jsx
@@ -0,0 +1,15 @@
+import HighlightText from "./HighlightText";
+import Button from "./Button";
+const CodeBlock = ({
+  position,
+  heading,
+  subHeading,
+  ctabtn1,
+  ctabtn2,
+  codeblock,
+  CodeColor,
+}) => {
+  return <div className={`flex ${position}`}></div>;
+};
+
+export default CodeBlock;
