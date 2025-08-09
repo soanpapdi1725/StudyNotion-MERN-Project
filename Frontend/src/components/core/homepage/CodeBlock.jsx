a1931234eea742e635808aade59f4c8e8d4177f2
Sonu Yadav <sy239370@gmail.com>
2025-08-09 21:47:29 +0530

    09/08/2025 - 1

--- Frontend/src/components/core/homepage/CodeBlock.jsx
+++ Frontend/src/components/core/homepage/CodeBlock.jsx
@@ -32,7 +32,25 @@ const CodeBlock = ({
           </Button>
         </div>
       </div>
-      <div>
+      {/* Section 2 codes */}
+      <div className="w-[50%]">
+        {/* background gradient oval shape */}
+        <div className="text-center flex flex-col w-[10%] text-richblack-400 font-inter font-bold">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+          <p>4</p>
+          <p>5</p>
+          <p>6</p>
+          <p>7</p>
+          <p>8</p>
+          <p>9</p>
+          <p>10</p>
+          <p>11</p>
+        </div>
+        <div className="w-[90%] flex flex-col">
+
+        </div>
       </div>
     </div>
   );
