d1e6ed81634e5f1640161d6e18d395ca4c872dae
Sonu Yadav <sy239370@gmail.com>
2025-08-11 12:25:44 +0530

    11/08/2025 - 2

--- Frontend/src/components/core/homepage/LearningLanguageSection.jsx
+++ Frontend/src/components/core/homepage/LearningLanguageSection.jsx
@@ -1,8 +1,22 @@
-
+import HighlightText from "./HighlightText";
+import CTAButton from "./Button";
 const LearningLanguageSection = () => {
   return (
-    <div>LearningLanguageSection</div>
-  )
-}
+    <div>
+      <div className="flex flex-col items-center">
+        {/* heading and subheading */}
+        <div>
+          <h1></h1>
+          <p></p>
+        </div>
 
-export default LearningLanguageSection
+        {/* images */}
+        <div></div>
+        {/* Button */}
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default LearningLanguageSection;
