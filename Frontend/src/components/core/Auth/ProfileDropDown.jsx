e4d7c88a46950148737fff262e1cef3c007c395b
Sonu Yadav <sy239370@gmail.com>
2025-08-15 23:07:58 +0530

    15/08/2025 - 3

--- Frontend/src/components/core/Auth/ProfileDropDown.jsx
+++ Frontend/src/components/core/Auth/ProfileDropDown.jsx
@@ -1,6 +1,8 @@
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
+import dummyImage from "../../../assets/Images/dummyProfile.jpg";
+import {AiOutlineCaretDown} from ""
 const ProfileDropDown = () => {
   const { user } = useSelector((state) => state.userType);
   const dispatch = useDispatch();
@@ -8,15 +10,18 @@ const ProfileDropDown = () => {
   const [open, setOpen] = useState(false);
   const ref = useRef(null);
   return (
-    <button className="relative"
+    <button
+      className="relative"
       onClick={() => {
         setOpen(!open);
       }}
     >
       {/* image and drop down wla div */}
-      <div>
-        <img {/**src={user?.image} */} src={}  alt={`profileImage${user?.firstName}`} />
+      <div className="flex flex-row items-center">
+        {/**src={user?.image} */}
+        <img className="object-cover w-[30px] rounded-full aspect-square" src={dummyImage} alt={`profileImage${user?.firstName}`} />
       </div>
+      <AiOutlineCaretDown />
     </button>
   );
 };
