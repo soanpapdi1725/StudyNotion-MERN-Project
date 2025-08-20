2ebffa0509e5c7d4f235a20613824c2c8f856c4a
Sonu Yadav <sy239370@gmail.com>
2025-08-21 00:31:01 +0530

    21/08/2025 - 2

--- backend-StudyNotion/controllers/ResetPassword.js
+++ backend-StudyNotion/controllers/ResetPassword.js
@@ -3,7 +3,7 @@ const mailsender = require("../utils/mailSender");
 const crypto = require("crypto");
 const bcryptjs = require("bcryptjs");
 const resetPasswordTemplate = require("../mail/templates/resetPassLink");
-const { passwordUpdated } = require("../mail/templates/passwordUpdate");
+const { passwordResetMail } = require("../mail/templates/PasswordResetMail");
 // Reset Password Token Controller
 exports.resetPassToken = async (req, res) => {
   try {
@@ -116,7 +116,7 @@ exports.resetPassword = async (req, res) => {
     await mailsender(
       user.email,
       "Password Changed Confirmation",
-      passwordUpdated(user.email, user.firstName)
+      passwordResetMail(user.email, user.firstName)
     );
     // response bhej denge
     return res.status(200).json({
