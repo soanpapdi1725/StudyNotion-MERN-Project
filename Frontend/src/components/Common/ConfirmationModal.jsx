0a9a217a296880c6c03ae29817550f4a8b1b0fa0
Sonu Yadav <sy239370@gmail.com>
2025-09-03 15:58:32 +0530

    03/9/2025 - 1

--- Frontend/src/components/Common/ConfirmationModal.jsx
+++ Frontend/src/components/Common/ConfirmationModal.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-const ConfirmationModal = () => {
+const ConfirmationModal = ({modalData}) => {
   return (
-    <div></div>
+    <div>
+      <div>
+        <p>
+          {modalData.text1}
+        </p>
+        <p>
+          {modalData.text2}
+        </p>
+        {/* Buttons */}
+        <div>
+          {/* IconButtons */}
+        </div>
+      </div>
+    </div>
   )
 }
 
