c24a435f7c3a5d46af327081b1cfd4205324a530
Sonu Yadav <sy239370@gmail.com>
2025-09-18 01:46:07 +0530

    minor bugs fixed

--- Frontend/src/components/core/Dashboard/Pages/Settings.jsx
+++ Frontend/src/components/core/Dashboard/Pages/Settings.jsx
@@ -18,10 +18,9 @@ const Settings = () => {
     reset,
     handleSubmit,
   } = useForm();
-
   const handleOnChangePreview = (event) => {
     const file = event.target.files?.[0];
-    if (!file) {
+    if (!file || file.size >= 2 * 1024 * 1024) {
       setImagePreview(null);
       return;
     }
@@ -31,17 +30,14 @@ const Settings = () => {
 
   const submitProfilePicture = async (formData) => {
     const file = formData.newUserImage[0]; // Get the file from the FileList
-
     if (!file) {
       return;
     }
-
     // Create FormData object for file upload
     const uploadData = new FormData();
     uploadData.append("newUserImage", file);
-
-
     dispatch(changeProfileImage(uploadData, setLoading));
+    setImagePreview(null);
   };
   if (loading) {
     return (
@@ -54,73 +50,75 @@ const Settings = () => {
       <>
         <div className="flex flex-col justify-center text-white my-20 gap-15 mx-auto w-11/12 max-w-[1000px]">
           <h1 className="text-4xl">Edit Profile</h1>
-          <div className="flex sm:flex-row flex-col  items-center gap-4 bg-richblack-800 w-full py-10 px-8 rounded-lg">
-            <div>
-              <img
-                className="rounded-full aspect-square w-[78px]"
-                src={user?.image}
-                alt={`${user?.firstName}'s image`}
-              />
-            </div>
-            <div className="flex flex-col gap-4 justify-center">
-              <h1 className="sm:text-2xl text-xl">Change Profile Picture</h1>
-              {/* buttons such as file or upload one */}
-              <form
-                className="flex flex-row gap-4 justify-center"
-                onSubmit={handleSubmit(submitProfilePicture)}
-              >
-                <label
-                  className="px-4 py-2 cursor-pointer text-lg font-semibold bg-richblack-700 border-[1px] border-richblack-100/10 text-white rounded-lg hover:bg-richblack-600 active:bg-richblack-500 duration-100 transition-all ease-in-out"
-                  htmlFor="file"
-                >
-                  Select
-                </label>
-                <input
-                  id="file"
-                  type="file"
-                  name="newUserImage"
-                  className="hidden"
-                  accept=".jpg, .jpeg, .png"
-                  placeholder="File"
-                  {...register("newUserImage", {
-                    onChange: handleOnChangePreview,
-                    required: true,
-                    validate: {
-                      acceptedFormats: (files) =>
-                        ["image/jpg", "image/jpeg", "image/png"].includes(
-                          files[0]?.type
-                        ) || "Only JPG/JPEG/PNG images are allowed",
-                      fileSize: (files) =>
-                        files[0]?.size <= 2 * 1024 * 1024 ||
-                        "Max file should be 2 MB only",
-                    },
-                  })}
+          <div className="flex flex-col gap-4 bg-richblack-800 w-full py-10 px-8 rounded-lg">
+            <div className="flex sm:flex-row flex-col items-center gap-4">
+              <div>
+                <img
+                  className="rounded-full aspect-square w-[78px]"
+                  src={user?.image}
+                  alt={`${user?.firstName}'s image`}
                 />
-                <button
-                  className="bg-yellow-50 px-4 py-2 hover:bg-yellow-100 active:bg-yellow-100 rounded-lg text-richblack-900 h-12 text-lg font-semibold duration-100 transition-all ease-in-out"
-                  type="submit"
+              </div>
+              <div className="flex flex-col gap-4 justify-center">
+                <h1 className="sm:text-2xl text-xl text-center">Change Profile Picture</h1>
+                {/* buttons such as file or upload one */}
+                <form
+                  className="flex flex-row gap-4 justify-center items-center"
+                  onSubmit={handleSubmit(submitProfilePicture)}
                 >
-                  Upload
-                </button>
-                {imagePreview && (
-                  <div
-                    onClick={(event) => {
-                      event.stopPropagation();
-                      setViewImagePreview(true);
-                    }}
-                    className="bg-suar-50 px-4 py-2 hover:bg-suar-100 active:bg-suar-100 rounded-lg text-richblack-900 h-12 text-lg font-semibold duration-100 transition-all ease-in-out"
+                  <label
+                    className="px-4 py-2 cursor-pointer text-lg font-semibold bg-richblack-700 border-[1px] border-richblack-100/10 text-white rounded-lg hover:bg-richblack-600 active:bg-richblack-500 duration-100 transition-all ease-in-out"
+                    htmlFor="file"
                   >
-                    See Preview
-                  </div>
-                )}
-
-                {errors.message && (
-                  <span className="text-pink-300 text-start text-sm">
-                    Please Select image Only in jpg, jpeg, png formats
-                  </span>
-                )}
-              </form>
+                    Select
+                  </label>
+                  <input
+                    id="file"
+                    type="file"
+                    name="newUserImage"
+                    className="hidden"
+                    accept=".jpg, .jpeg, .png"
+                    placeholder="File"
+                    {...register("newUserImage", {
+                      onChange: handleOnChangePreview,
+                      required: true,
+                      validate: {
+                        acceptedFormats: (files) =>
+                          ["image/jpg", "image/jpeg", "image/png"].includes(
+                            files[0]?.type
+                          ) || "Only JPG/JPEG/PNG images are allowed",
+                        fileSize: (files) =>
+                          files[0]?.size <= 2 * 1024 * 1024 ||
+                          "Max file should be 2 MB only",
+                      },
+                    })}
+                  />
+                  <button
+                    className="bg-yellow-50 px-4 py-2 hover:bg-yellow-100 active:bg-yellow-100 rounded-lg text-richblack-900 h-12 text-lg font-semibold duration-100 transition-all ease-in-out"
+                    type="submit"
+                  >
+                    Upload
+                  </button>
+                  {imagePreview && (
+                    <div
+                      onClick={(event) => {
+                        event.stopPropagation();
+                        setViewImagePreview(true);
+                      }}
+                      className="bg-suar-50 px-4 py-2 hover:bg-suar-100 active:bg-suar-100 rounded-lg text-richblack-900 h-12 text-lg font-semibold duration-100 transition-all ease-in-out"
+                    >
+                      See Preview
+                    </div>
+                  )}
+                </form>
+              </div>
             </div>
+
+            {errors.newUserImage && (
+              <span className="text-pink-300 text-center md:text-start text-sm">
+                {errors.newUserImage.message || "Please Upload image"}
+              </span>
+            )}
           </div>
         </div>
         {viewImagePreview && (
