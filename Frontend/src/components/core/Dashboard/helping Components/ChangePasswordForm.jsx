4175a438ab3dfc6b53124f30a353c38af5b5b6cc
Sonu Yadav <sy239370@gmail.com>
2025-10-28 16:17:50 +0530

    input section completed

--- Frontend/src/components/core/Dashboard/helping Components/ChangePasswordForm.jsx
+++ Frontend/src/components/core/Dashboard/helping Components/ChangePasswordForm.jsx
@@ -5,30 +5,30 @@ const ChangePasswordForm = () => {
   const [seePass_CN, setSeePass_CN] = useState(false);
 
   return (
-    <form className="w-full">
-      <div className="flex-col">
+    <form className="w-full flex flex-col gap-4">
+      <div className="flex flex-col gap-1">
         <label htmlFor="CurrPassword">Current Password</label>
         <input
           id="CurrPassword"
-          className="bg-richblack-500"
+          className="bg-richblack-700 h-10 shadow-[0px_0.9px_0.5px_0.2px_rgba(255,255,255,0.3)] text-pure-greys-5 focus:border-none   border-none text-lg px-2 py-2.5 rounded-lg "
           type={`${seePass_C ? "text" : "password"}`}
           placeholder="Enter Your Current Password"
         />
       </div>
-      <div className="flex-col">
+      <div className="flex flex-col gap-1">
         <label htmlFor="NewPassword">New Password</label>
         <input
           id="NewPassword"
-          className="bg-richblack-500"
+          className="bg-richblack-700 h-10 shadow-[0px_0.9px_0.5px_0.2px_rgba(255,255,255,0.3)] text-pure-greys-5 focus:border-none   border-none text-lg px-2 py-2.5 rounded-lg "
           type={`${seePass_N ? "text" : "password"}`}
           placeholder="Enter Your New Password"
         />
       </div>
-      <div className="flex-col">
+      <div className="flex flex-col gap-1">
         <label htmlFor="CNPassword">Confirm New Password</label>
         <input
           id="CNPassword"
-          className="bg-richblack-500"
+          className="bg-richblack-700 h-10 shadow-[0px_0.9px_0.5px_0.2px_rgba(255,255,255,0.3)] text-pure-greys-5 focus:border-none   border-none text-lg px-2 py-2.5 rounded-lg "
           type={`${seePass_CN ? "text" : "password"}`}
           placeholder="Confirm Your New Password"
         />
