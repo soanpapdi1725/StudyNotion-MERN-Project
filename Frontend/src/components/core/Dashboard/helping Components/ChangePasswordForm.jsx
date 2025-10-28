4299923a803765e857d69e7ffc56f96e1187a713
Sonu Yadav <sy239370@gmail.com>
2025-10-28 16:12:46 +0530

    adding Change password section in setting

--- Frontend/src/components/core/Dashboard/helping Components/ChangePasswordForm.jsx
+++ Frontend/src/components/core/Dashboard/helping Components/ChangePasswordForm.jsx
@@ -1,10 +1,38 @@
-import React from "react";
-
+import { useState } from "react";
 const ChangePasswordForm = () => {
+  const [seePass_C, setSeePass_C] = useState(false);
+  const [seePass_N, setSeePass_N] = useState(false);
+  const [seePass_CN, setSeePass_CN] = useState(false);
+
   return (
     <form className="w-full">
-      <label htmlFor="CurrPassword">Current Password</label>
-      <input type="text" />
+      <div className="flex-col">
+        <label htmlFor="CurrPassword">Current Password</label>
+        <input
+          id="CurrPassword"
+          className="bg-richblack-500"
+          type={`${seePass_C ? "text" : "password"}`}
+          placeholder="Enter Your Current Password"
+        />
+      </div>
+      <div className="flex-col">
+        <label htmlFor="NewPassword">New Password</label>
+        <input
+          id="NewPassword"
+          className="bg-richblack-500"
+          type={`${seePass_N ? "text" : "password"}`}
+          placeholder="Enter Your New Password"
+        />
+      </div>
+      <div className="flex-col">
+        <label htmlFor="CNPassword">Confirm New Password</label>
+        <input
+          id="CNPassword"
+          className="bg-richblack-500"
+          type={`${seePass_CN ? "text" : "password"}`}
+          placeholder="Confirm Your New Password"
+        />
+      </div>
     </form>
   );
 };
