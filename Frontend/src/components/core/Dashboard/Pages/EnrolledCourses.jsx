ae2a8c1e2e822b599953a7249034155483daa1e4
Sonu Yadav <sy239370@gmail.com>
2026-01-18 23:38:31 +0530

    19/01/2026

--- Frontend/src/components/core/Dashboard/Pages/EnrolledCourses.jsx
+++ Frontend/src/components/core/Dashboard/Pages/EnrolledCourses.jsx
@@ -39,19 +39,17 @@ const EnrolledCourses = () => {
         <p>You Have Not Enrolled in Any Course yet</p>
       ) : (
         <div className="border-1 gap-y-5 border-richblack-600 rounded-md overflow-hidden">
-          <div className="flex flex-row justify-evenly gap-5 w-[100%] bg-richblack-500 py-2.5">
-            <div className="w-12 text-center">S. No</div>
-            <div className="w-[40%]  text-center">Courses</div>
-            <div className="w-[10%]  text-center">Duration</div>
-            <div className="w-[20%]  text-center">Progress</div>
+          <div className="flex flex-row justify-between px-4 gap-5 w-[100%] bg-richblack-700 py-2.5">
+            <div className="w-[40%] text-richblack-50 text-start">Course Name</div>
+            <div className="w-[10%] text-richblack-50 text-start">Durations</div>
+            <div className="w-[20%] text-richblack-50 text-start">Progress</div>
           </div>
           {enrolledCourses.map((course, index) => (
             <div
               key={index}
-              className={`flex flex-row justify-evenly ${!index === enrolledCourses.length - 1 ? "" : "border-b-1"} border-richblack-600 gap-5 w-[100%] py-2.5`}
+              className={`flex px-4 flex-row ${!index === enrolledCourses.length - 1 ? "" : "border-b-1"} border-richblack-600 gap-5 w-[100%] py-2.5`}
             >
-              <div className="w-12 text-center">{index + 1}</div>
-              <div className="w-[40%] flex flex-row">
+              <div className="w-[40%]  flex flex-row">
                 {/* Image */}
                 <div>
                   <img
@@ -60,10 +58,13 @@ const EnrolledCourses = () => {
                     height={70}
                     width={70}
                     loading="lazy"
-                    className="rounded-md"
+                    className="rounded-md aspect-square mx-4"
                   />
                 </div>
                 {/* CourseName */}
+                <h1>
+                  {course.courseName}
+                </h1>
               </div>
               <div className="w-[10%]  text-center"></div>
               <div className="w-[20%]  text-center"></div>
