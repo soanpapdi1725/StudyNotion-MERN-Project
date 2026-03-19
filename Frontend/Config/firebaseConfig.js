d6d600e6a435aaaae938de915fbe2a8d3c64f345
Sonu Yadav <sy239370@gmail.com>
2026-03-20 02:45:51 +0530

    firebase setup completed now

--- Frontend/Config/firebaseConfig.js
+++ Frontend/Config/firebaseConfig.js
@@ -1,25 +1,25 @@
-// getAuth coming from firebase/auth
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "studynotion-2517.firebaseapp.com",
-  projectId: "studynotion-2517",
-  storageBucket: "studynotion-2517.firebasestorage.app",
-  messagingSenderId: "804610072487",
-  appId: "1:804610072487:web:cbc379c754cbae277c6082",
+apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "studynotion-2517-2517.firebaseapp.com",
+  projectId: "studynotion-2517-2517",
+  storageBucket: "studynotion-2517-2517.firebasestorage.app",
+  messagingSenderId: "1069796136433",
+  appId: "1:1069796136433:web:419dc397da9619a9aa6050",
+  measurementId: "G-49KGQF29RY"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 // exporting auth and Provider
-export { auth, provider };
+export { auth, provider, analytics };
