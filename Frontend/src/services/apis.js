b25b335bf16be83443772fc39e77000955440c00
Sonu Yadav <sy239370@gmail.com>
2026-03-20 18:51:07 +0530

    checking why backend not working

--- Frontend/src/services/apis.js
+++ Frontend/src/services/apis.js
@@ -1,6 +1,7 @@
 const BASE_URL =
   import.meta.env.VITE_BACKEND_BASE_URL_VERCEL || import.meta.env.VITE_BASE_URL;
 
+console.log(import.meta.env.VITE_BACKEND_BASE_URL_VERCEL);
 export const courseEndpoints = {
   GET_ALL_CATEGORIES_API: BASE_URL + "/course/getAllCategories",
 };
